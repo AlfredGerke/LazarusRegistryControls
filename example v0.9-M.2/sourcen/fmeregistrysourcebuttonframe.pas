4330af3a7712da332b492000e5f4c7d8348210e0
AlfredGerke <alfred.gerke@software-aus-erwitte.de>
2015-08-26 22:13:36 +0200

    s. Issue #31
    ...

--- example v0.9-M.2/sourcen/fmeregistrysourcebuttonframe.pas
+++ example v0.9-M.2/sourcen/fmeregistrysourcebuttonframe.pas
@@ -9,12 +9,19 @@ uses
   SysUtils,
   FileUtil,
   Forms,
-  Controls;
+  Controls, ActnList, StdCtrls;
 
 type
+
+  { TTRegistrySoruceButtonFrame }
+
   TTRegistrySoruceButtonFrame = class(TFrame)
+    acList: TActionList;
+    acCreateSettings: TAction;
+    btnCreateSettings: TButton;
+    procedure acCreateSettingsExecute(Sender: TObject);
   private
-  public
+    procedure CreateRegistrySettings;
   end;
 
 implementation
@@ -23,6 +30,85 @@ implementation
 
 uses
   datregistry;
+
+{ TTRegistrySoruceButtonFrame }
+
+procedure TTRegistrySoruceButtonFrame.acCreateSettingsExecute(Sender: TObject);
+begin
+  CreateRegistrySettings;
+end;
+
+procedure TTRegistrySoruceButtonFrame.CreateRegistrySettings;
+begin
+  Screen.Cursor := crHourGlass;
+
+  with RegistrySourceModule.rsRegistrySource do
+  begin
+    DoSyncData := False;
+    WriteDefaults := True;
+
+    WriteString('About', 'Version', '1.0.0');
+    WriteString('About', 'Projekt', 'LazarusRegistryControls');
+    WriteString('About', 'Git',
+      'https://github.com/AlfredGerke/LazarusRegistryControls.git');
+
+    WriteString('TRegEdit', 'Key', 'Value');
+    WriteString('TRegLabel', 'Key', 'Standardcaption');
+
+    WriteBool('TRegCheckBox', 'Key', True);
+    WriteString('TRegCheckBox', 'Caption', 'Standardcaption');
+
+    WriteBool('TRegRadioButton', 'Key', True);
+    WriteString('TRegRadioButton', 'Caption', 'Standardcaption');
+
+    WriteString('TRegListBoxItems', 'Key1', 'Value1');
+    WriteString('TRegListBoxItems', 'Key2', 'Value2');
+    WriteString('TRegListBoxItems', 'Key3', 'Value3');
+    WriteString('TRegListBoxItems', 'Key4', 'Value4');
+    WriteString('TRegListBoxItems', 'Key5', 'Value5');
+    WriteInteger('TRegListBox', 'Index', 0);
+
+    WriteString('TRegComboBoxItems', 'Key1', 'Value1');
+    WriteString('TRegComboBoxItems', 'Key2', 'Value2');
+    WriteString('TRegComboBoxItems', 'Key3', 'Value3');
+    WriteString('TRegComboBoxItems', 'Key4', 'Value4');
+    WriteString('TRegComboBoxItems', 'Key5', 'Value5');
+    WriteInteger('TRegComboBox', 'Index', 0);
+
+    WriteString('TRegValueListEditorItems', 'Key1', 'Value1');
+    WriteString('TRegValueListEditorItems', 'Key2', 'Value2');
+    WriteString('TRegValueListEditorItems', 'Key3', 'Value3');
+    WriteString('TRegValueListEditorItems', 'Key4', 'Value4');
+    WriteString('TRegValueListEditorItems', 'Key5', 'Value5');
+
+    WriteString('TRegRadioGroupItems', 'Key1', 'Value1');
+    WriteString('TRegRadioGroupItems', 'Key2', 'Value2');
+    WriteString('TRegRadioGroupItems', 'Key3', 'Value3');
+    WriteString('TRegRadioGroupItems', 'Key4', 'Value4');
+    WriteString('TRegRadioGroupItems', 'Key5', 'Value5');
+    WriteInteger('TRegRadioGroup', 'index', 0);
+    WriteString('TRegRadioGroup', 'Caption', 'Standardcaption');
+
+    WriteBool('TRegCheckListBox', 'Key1', True);
+    WriteBool('TRegCheckListBox', 'Key2', True);
+    WriteBool('TRegCheckListBox', 'Key3', True);
+    WriteBool('TRegCheckListBox', 'Key4', True);
+    WriteBool('TRegCheckListBox', 'Key5', True);
+
+    WriteBool('TRegCheckGroupBoxItems', 'Key1', True);
+    WriteBool('TRegCheckGroupBoxItems', 'Key1', True);
+    WriteBool('TRegCheckGroupBoxItems', 'Key1', True);
+    WriteBool('TRegCheckGroupBoxItems', 'Key1', True);
+    WriteBool('TRegCheckGroupBoxItems', 'Key1', True);
+    WriteInteger('TRegCheckGroupBox', 'Index', 0);
+    WriteString('TRegCheckGroupBox', 'Caption', 'Standardcaption');
+
+    DoSyncData := True;
+    WriteDefaults := False;
+  end;
+
+  Screen.Cursor := crDefault;
+end;
 
 end.
 
